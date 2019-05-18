fb3437f3fc8ae9dffe893d5e38db79a2e6252650
adan <adan@adanui-MacBookPro.local>
2019-05-19 00:36:07 +0900

    ##1.2 Pre Loading Assets"

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppLoading, Font, Asset } from "expo";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
+  state = {
+    loaded: false
+  };
+  handleError = error => {
+    console.log(error);
+  };
+
+  handleLoaded = () => {
+    this.setState({ loaded: true });
+  };
+
+  loadAssets = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    });
+    //await Asset.loadAsync([require("")]);
+  };
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    const { loaded } = this.state;
+    if (loaded) {
+      return (
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      );
+    } else {
+      return (
+        <AppLoading
+          startAsyn={this.loadAssets}
+          onFinish={this.handleLoaded}
+          onError={this.handleError}
+        />
+      );
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
